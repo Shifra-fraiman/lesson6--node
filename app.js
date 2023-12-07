eae4563cad1a351fa98c386b024e29bd2e95b93a
shifra-fraiman <s0548549680@gmail.com>
2023-12-08 01:42:48 +0200

    lesson 8

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app= express();
 app.use(workers);
 app.use(coursess);
 
+// app.use(coursess.json())
+// app.use(coursess.urlencoded({ extended: false }))
 
 app.listen(3000, () => { 
     console.log("listening on 3000 server"); 
