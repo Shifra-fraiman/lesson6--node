75a4d1cf392cdbbcd4fd084b5072f64d5ba13f7f
shifra-fraiman <s0548549680@gmail.com>
2023-12-08 01:44:09 +0200

    lesson 8-put, delete, post

--- controllers/coursesscontroller.js
+++ controllers/coursesscontroller.js
@@ -2,6 +2,15 @@ const coursess = require('../Data/coursess.json');
 const { Router } = require('express'); 
 const app = Router();
 
+//lesson 8
+//bodyParser
+const bodyParser= require('body-parser');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+//fs- to read and write file
+const fs= require('fs');
+const fsPromises= require('fs').promises;
+
 app.get("/coursess", (req, res)=>{
     res.json(coursess);
 })
@@ -30,5 +39,74 @@ app.get("*", (req, res)=>{
     res.status(404);
     res.send(`Eror status: ${res.statusCode}. You have an error in the URL`);
 });
+
+
+//lesson 8
+app.post("/coursess", async(req, res)=>{
+    const data= req.body;
+    try{
+        const dataObject= {
+            code: data.code,
+            description: data.description
+        }
+        const allPrevData=(await fsPromises.readFile( "./Data/coursess.json"));
+        const allData= JSON.parse(allPrevData);
+        allData.push(dataObject);
+
+        try { 
+            await fsPromises.writeFile("./Data/coursess.json", JSON.stringify(allData))
+            res.send(fs.readFileSync("./Data/coursess.json"));
+        } catch (err) { 
+            console.error(err); 
+        } 
+    }
+    catch(err){
+        console.log(err);
+    }  
+})
+
+app.delete("/coursess", async(req, res)=>{
+   
+    try{
+        const code= req.body.code;
+        console.log(code);
+        const allPrev=(await fsPromises.readFile( "./Data/coursess.json"));
+        const allPrevData= JSON.parse(allPrev);
+        const allDataAfterDelete=allPrevData.filter(course=> course.code!=parseInt(code));
+    
+        try { 
+            await fsPromises.writeFile("./Data/coursess.json", JSON.stringify(allDataAfterDelete))
+            res.send(fs.readFileSync("./Data/coursess.json"));
+        } catch (err) { 
+            console.error(err); 
+        } 
+    }
+    catch(err){
+        console.log(err);
+    }  
+})
+
+app.put("/coursess", async(req, res)=>{
+   
+    try{
+        const update= req.body;
+        
+        const allPrev=(await fsPromises.readFile( "./Data/coursess.json"));
+        const allPrevData= JSON.parse(allPrev);
+        const allDataAfterUpdate=allPrevData.map(course=> course.code===parseInt(update.code)? {code: update.code, description: update.description} :course);
+    
+        try { 
+            await fsPromises.writeFile("./Data/coursess.json", JSON.stringify(allDataAfterUpdate))
+            res.send(fs.readFileSync("./Data/coursess.json"));
+        } catch (err) { 
+            console.error(err); 
+        } 
+    }
+    catch(err){
+        console.log(err);
+    }  
+})
+
+
 
 module.exports = app;
